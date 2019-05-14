22c7362278136a0fa8de509354793c6d678fcc01
kain <zhangtqx@vip.qq.com>
2019-05-14 14:41:49 +0800

    Submit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <p>This is an about page</p>
+        <nut-button @click="test">
+            去结算
+        </nut-button>
+    </div>
 </template>
+
+<script lang="ts">
+    import {Vue, Component} from "vue-property-decorator";
+
+    @Component
+    export default class About extends Vue {
+        test() {
+            this.$dialog({
+                title: "asd",
+                content: "xxx",
+                onOkBtn() {
+                    this.close();
+                }
+            });
+        }
+    }
+</script>
