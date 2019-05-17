378bc019e891b737d5a704e4fe13f3e6b0115939
kain <zhangtqx@vip.qq.com>
2019-05-17 18:05:49 +0800

    PreTest Develop

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,55 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  hidden = false;
+  fullScreen = false;
+  topFlag = false;
+  tintColor = '#108ee9';
+  unselectedTintColor = '#888';
+  tabbarStyle: any = {height: '400px'};
+  selectedIndex = 1;
 
-  constructor() {
+  showTabBar(event) {
+    event.preventDefault();
+    this.hidden = !this.hidden;
   }
 
-  ngOnInit() {
+  showNextTabBar(event) {
+    event.preventDefault();
+    const PANE_COUNT = 4;
+    if (this.selectedIndex === PANE_COUNT - 1) {
+      this.selectedIndex = 0;
+    } else {
+      this.selectedIndex++;
+    }
+    console.log('selectedIndex: ', this.selectedIndex);
+  }
+
+  showFullScreen(event) {
+    event.preventDefault();
+    this.fullScreen = !this.fullScreen;
+    this.tabbarStyle = this.fullScreen
+      ? {
+        position: 'fixed',
+        height: '100%',
+        width: '100%',
+        top: 0
+      }
+      : {height: '400px'};
+  }
+
+  changePosition(event) {
+    event.preventDefault();
+    this.topFlag = !this.topFlag;
+  }
+
+  tabBarTabOnPress(pressParam: any) {
+    console.log('onPress Params: ', pressParam);
+    this.selectedIndex = pressParam.index;
   }
 }
