8651ac6645726c8493de80bcf8a6d6b2dcbbf40c
kain <zhangtqx@vip.qq.com>
2019-05-18 21:27:47 +0800

    Submit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,50 +6,9 @@ import {Component} from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  hidden = false;
-  fullScreen = false;
-  topFlag = false;
-  tintColor = '#108ee9';
-  unselectedTintColor = '#888';
-  tabbarStyle: any = {height: '400px'};
-  selectedIndex = 1;
-
-  showTabBar(event) {
-    event.preventDefault();
-    this.hidden = !this.hidden;
-  }
-
-  showNextTabBar(event) {
-    event.preventDefault();
-    const PANE_COUNT = 4;
-    if (this.selectedIndex === PANE_COUNT - 1) {
-      this.selectedIndex = 0;
-    } else {
-      this.selectedIndex++;
-    }
-    console.log('selectedIndex: ', this.selectedIndex);
-  }
-
-  showFullScreen(event) {
-    event.preventDefault();
-    this.fullScreen = !this.fullScreen;
-    this.tabbarStyle = this.fullScreen
-      ? {
-        position: 'fixed',
-        height: '100%',
-        width: '100%',
-        top: 0
-      }
-      : {height: '400px'};
-  }
-
-  changePosition(event) {
-    event.preventDefault();
-    this.topFlag = !this.topFlag;
-  }
+  selectedIndex = 0;
 
   tabBarTabOnPress(pressParam: any) {
-    console.log('onPress Params: ', pressParam);
     this.selectedIndex = pressParam.index;
   }
 }
