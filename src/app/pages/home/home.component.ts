1b6fdf126a29743f5755e8a49233f5efb4cd131a
kain <zhangtqx@vip.qq.com>
2019-05-20 09:30:36 +0800

    服务测试

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,9 +6,18 @@ import {Component} from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  title = '服务测试';
   selectedIndex = 0;
 
   tabBarTabOnPress(pressParam: any) {
+    switch (pressParam.index) {
+      case 0:
+        this.title = '服务测试';
+        break;
+      case 1:
+        this.title = '场景模拟';
+        break;
+    }
     this.selectedIndex = pressParam.index;
   }
 }
