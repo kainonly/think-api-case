cd2bd940c0f93cf39abd2aa5b9b7f8eba679cdce
kain <zhangtqx@vip.qq.com>
2019-10-31 11:59:07 +0800

    Submit Testing

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -3,10 +3,18 @@
 namespace app\index\controller;
 
 use app\common\BaseController;
+use think\App;
+use think\Container;
+use think\extra\contract\UtilsInterface;
+use think\extra\service\UtilsService;
 
 class Index extends BaseController
 {
     public function index()
     {
+        $app = new App();
+        $app->register(UtilsService::class);
+        $utils = $app->get(UtilsInterface::class);
+        dump($utils->uuid());
     }
 }
