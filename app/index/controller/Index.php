e0f8e613ccc631f83730aa211a659f5dc3acb0df
kain <zhangtqx@vip.qq.com>
2019-10-31 10:23:27 +0800

    Submit Extra

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -3,13 +3,10 @@
 namespace app\index\controller;
 
 use app\common\BaseController;
-use think\extra\contract\CipherInterface;
 
 class Index extends BaseController
 {
-    public function index(CipherInterface $cipher)
+    public function index()
     {
-        $context = $cipher->encrypt(['name' => 'kain']);
-        dump($context);
     }
 }
