f17dd7d1d10b26bee7e4864101cb2ae4af9caaed
kain <zhangtqx@vip.qq.com>
2019-10-31 17:20:42 +0800

    Submit Facade

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -4,29 +4,12 @@ namespace app\index\controller;
 
 use app\common\BaseController;
 use think\App;
-use think\extra\contract\HashInterface;
-use think\extra\service\HashService;
+use think\support\facade\Utils;
 
 class Index extends BaseController
 {
     public function index()
     {
-        $app = new App();
-        $app->config->set([
-            'driver' => 'argon2id',
-            'bcrypt' => [
-                'rounds' => env('BCRYPT_ROUNDS', 10),
-            ],
-            'argon' => [
-                'memory' => 1024,
-                'threads' => 2,
-                'time' => 2,
-            ],
-        ], 'hashing');
-        dump($app->config);
-        $app->register(HashService::class);
-        $hash = $app->get(HashInterface::class);
-        $data = $hash->create('cxc');
-        dump($data);
+        dump(Utils::uuid());
     }
 }
