48ebeb48303087b13ea9ff2b292d12940eb403e7
kain <zhangtqx@vip.qq.com>
2019-12-23 18:17:18 +0800

    更新 think-extra

--- config/token.php
+++ config/token.php
@@ -1,8 +1,13 @@
 <?php
 return [
+    'default' => [
+        'issuer' => 'default',
+        'audience' => 'everyone',
+        'expires' => 3600
+    ],
     'system' => [
         'issuer' => 'system',
         'audience' => 'everyone',
         'expires' => 3600
-    ]
+    ],
 ];
