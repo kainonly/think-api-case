db7ead1e8536a602dc98e0980460f2a1fa367432
kain <zhangtqx@vip.qq.com>
2019-12-25 12:00:00 +0800

    更新ElasticSearch

--- config/database.php
+++ config/database.php
@@ -73,5 +73,27 @@ return [
             // 数据库号
             'database' => Env::get('redis.db', 0),
         ]
+    ],
+
+    // elasticsearch 设置
+    'elasticsearch' => [
+        'default' => [
+            // 集群连接
+            'hosts' => explode(',', Env::get('elasticsearch.hosts', 'localhost:9200')),
+            // 重试次数
+            'retries' => 0,
+            // 公共CA证书
+            'SSLVerification' => null,
+            // 开启日志
+            'logger' => null,
+            // 配置 HTTP Handler
+            'handler' => null,
+            // 设置连接池
+            'connectionPool' => Elasticsearch\ConnectionPool\StaticNoPingConnectionPool::class,
+            // 设置选择器
+            'selector' => Elasticsearch\ConnectionPool\Selectors\RoundRobinSelector::class,
+            // 设置序列化器
+            'serializer' => Elasticsearch\Serializers\SmartSerializer::class
+        ]
     ]
 ];
