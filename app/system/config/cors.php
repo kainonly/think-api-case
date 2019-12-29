9c66997ecdb73f0160df548f4d1ea3afd4675466
kain <zhangtqx@vip.qq.com>
2019-12-29 18:26:16 +0800

    开放OPTIONS

--- app/system/config/cors.php
+++ app/system/config/cors.php
@@ -5,7 +5,7 @@ use think\facade\Env;
 return [
     'allow_origin' => explode(',', Env::get('cors.system')),
     'allow_credentials' => true,
-    'allow_methods' => ['POST'],
+    'allow_methods' => ['POST', 'OPTIONS'],
     'expose_headers' => [],
     'allow_headers' => ['CONTENT-TYPE', 'X-REQUESTED-WITH'],
     'max_age' => 31536000,
