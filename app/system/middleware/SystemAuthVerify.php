16a1c7a262603a1f8178e5047e357e25edbe5eee
kain <zhangtqx@vip.qq.com>
2020-06-19 17:28:50 +0800

    update SystemAuthVerify

--- app/system/middleware/SystemAuthVerify.php
+++ app/system/middleware/SystemAuthVerify.php
@@ -3,9 +3,24 @@ declare (strict_types=1);
 
 namespace app\system\middleware;
 
+use app\system\redis\AdminRedis;
+use stdClass;
 use think\support\middleware\AuthVerify;
 
 class SystemAuthVerify extends AuthVerify
 {
     protected $scene = 'system';
+
+    protected function hook(stdClass $symbol): bool
+    {
+        $data = AdminRedis::create()->get($symbol->username);
+        if (empty($data)) {
+            $this->hookResult = [
+                'error' => 1,
+                'msg' => 'freeze'
+            ];
+            return false;
+        }
+        return true;
+    }
 }
