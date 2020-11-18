4669ab6c140ad405b928105287dc1c2dfcbb2277
kain <zhangtqx@vip.qq.com>
2020-11-18 15:02:37 +0800

    update

--- config/aliyun.php
+++ config/aliyun.php
@@ -4,6 +4,7 @@ return [
     'accessKeySecret' => env('aliyun.secret'),
     'oss' => [
         'endpoint' => env('aliyun.oss_endpoint'),
+        'extranet' => env('aliyun.oss_extranet'),
         'bucket' => env('aliyun.oss_bucket')
     ]
 ];
