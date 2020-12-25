68947e2fd71adbef35805bec2ab335ed096439af
kain <zhangtqx@vip.qq.com>
2020-12-25 09:53:52 +0800

    更新

--- app/service.php
+++ app/service.php
@@ -5,9 +5,9 @@ return [
     think\extra\service\CipherService::class,
     think\extra\service\TokenService::class,
     think\extra\service\UtilsService::class,
-    think\redis\service\RedisService::class,
-    think\amqp\service\AMQPService::class,
-    think\elastic\service\ElasticService::class,
+    think\redis\RedisService::class,
+    think\amqp\AMQPService::class,
+    think\elastic\ElasticService::class,
     think\aliyun\extra\OssService::class,
     think\huaweicloud\extra\ObsService::class,
     think\qcloud\extra\CosService::class
