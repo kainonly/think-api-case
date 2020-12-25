e4f01a1eb6ae6bb9103ba50476171ac588b5ca1c
kain <zhangtqx@vip.qq.com>
2020-12-25 17:23:33 +0800

    更新

--- app/index/controller/IndexController.php
+++ app/index/controller/IndexController.php
@@ -5,6 +5,8 @@ namespace app\index\controller;
 
 use app\common\BaseController;
 use think\App;
+use think\facade\Filesystem;
+use think\facade\Request;
 use think\Response;
 
 class IndexController extends BaseController
@@ -13,6 +15,25 @@ class IndexController extends BaseController
     {
         return json([
             'version' => app()->version(),
+        ]);
+    }
+
+    public function upload(): Response
+    {
+        $file = Request::file('file');
+        $fileName = date('Ymd') . '/' .
+            uuid()->toString() . '.' .
+            $file->getOriginalExtension();
+        Filesystem::disk('public')->putFileAs(
+            date('Ymd'),
+            $file,
+            uuid()->toString() . '.' . $file->getOriginalExtension()
+        );
+        return json([
+            'error' => 0,
+            'data' => [
+                'save_name' => $fileName,
+            ]
         ]);
     }
 }
