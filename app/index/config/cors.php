deb3b9767fdfdffe0b1e5ad4dd7364907ea8134f
kain <zhangtqx@vip.qq.com>
2021-01-27 17:38:34 +0800

    更新对象存储测试

--- app/index/config/cors.php
+++ app/index/config/cors.php
@@ -3,7 +3,7 @@ declare (strict_types=1);
 return [
     'allow_origin' => ['*'],
     'allow_credentials' => false,
-    'allow_methods' => ['POST', 'OPTIONS'],
+    'allow_methods' => ['GET', 'POST', 'OPTIONS'],
     'expose_headers' => [],
     'allow_headers' => ['CONTENT-TYPE', 'X-REQUESTED-WITH'],
     'max_age' => 31536000,
