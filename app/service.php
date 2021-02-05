db8d33f97688959149741cdd2e2d6bef27e0e270
kain <zhangtqx@vip.qq.com>
2021-02-05 16:50:01 +0800

    更新

--- app/service.php
+++ app/service.php
@@ -6,8 +6,6 @@ return [
     think\extra\service\TokenService::class,
     think\extra\service\UtilsService::class,
     think\redis\RedisService::class,
-    think\amqp\AMQPService::class,
-    think\elastic\ElasticService::class,
     think\aliyun\extra\OssService::class,
     think\huaweicloud\extra\ObsService::class,
     think\qcloud\extra\CosService::class
