52ad7ad389e7e354b522125c8a2ef6769653ee75
kain <zhangtqx@vip.qq.com>
2021-04-02 16:16:16 +0800

    更新

--- config/token.php
+++ config/token.php
@@ -1,13 +1,8 @@
 <?php
 return [
-    'default' => [
-        'issuer' => 'api.kainonly.com',
-        'audience' => '*',
-        'expires' => 3600
-    ],
     'system' => [
         'issuer' => 'api.kainonly.com',
         'audience' => 'console.kainonly.com',
-        'expires' => 3600
+        'expires' => 900
     ],
 ];
